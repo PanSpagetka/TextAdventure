9d99d2406f5ddc7382f1370de96daadeb1aeaa91
PanSpagetka <rknaur@redhat.com>
2017-02-08 18:37:20 +0100

    Remove things

--- main.rb
+++ main.rb
@@ -3,17 +3,9 @@ require 'require_all'
 require 'pry'
 require_all 'lib'
 
-#g = Game.new
-#g.start
 world = World.new
-#root = world.loadFromYAML('data/world.yaml')
 root = world.get_world
-#drawGraph(world.root)
-#world.root.children[0].children[0].moveNode(world.root.children[1])
-#drawGraph(world.root)
-#x = world.root.findNodeWithName('paper', world.root)
-#x
-#drawGraph(world.root)
+
 
 player = Player.new(nil, {:name => 'Pepa'})
 game = Game.new(world, player)
